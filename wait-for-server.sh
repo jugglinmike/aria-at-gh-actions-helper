b7a7fea6afe4501ceb5f4e7456db448d9d41cc8e
Mike Pennisi <mike@mikepennisi.com>
2024-05-01 19:09:35 -0400

    Correct logic and improve visibility

--- wait-for-server.sh
+++ wait-for-server.sh
@@ -15,20 +15,22 @@ trap clean_up EXIT
 for attempt in $(seq ${max_attempts}); do
   echo Trying ${attempt} of ${max_attempts}.
 
-  ./bin/at-driver serve > ${temp_file} &
+  ./bin/at-driver serve 2> ${temp_file} &
   sleep 3
   say This text came from the say command.
   sleep 1
   kill %%
 
   if grep --silent 'This text came from the say command.' ${temp_file}; then
+    echo The server is ready.
     break
   fi
+
+  echo The server is not ready. Text observed:
+  cat ${temp_file}
 
   if [ ${attempt} == ${max_attempts} ]; then
     echo Failed after ${attempt} attempts. >&2
     exit 1
   fi
 done
-
-echo The server is ready.
