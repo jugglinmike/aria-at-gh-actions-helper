16828a125f3ee937e4a02b68e6594762a8b0db0c
Mike Pennisi <mike@mikepennisi.com>
2024-05-01 19:17:58 -0400

    Improve output

--- wait-for-server.sh
+++ wait-for-server.sh
@@ -5,21 +5,19 @@ set -e
 max_attempts=20
 temp_file=$(mktemp)
 
-echo temp file: ${temp_file}
-
 function clean_up {
   rm ${temp_file}
 }
 trap clean_up EXIT
 
 for attempt in $(seq ${max_attempts}); do
-  echo Trying ${attempt} of ${max_attempts}.
+  echo "Testing for server availability (trial ${attempt} of ${max_attempts})."
 
   ./bin/at-driver serve 2> ${temp_file} &
   sleep 3
   say This text came from the say command.
   sleep 1
-  kill %%
+  kill -9 %%
 
   if grep --silent 'This text came from the say command.' ${temp_file}; then
     echo The server is ready.
@@ -27,7 +25,9 @@ for attempt in $(seq ${max_attempts}); do
   fi
 
   echo The server is not ready. Text observed:
-  cat ${temp_file}
+  echo
+  cat ${temp_file} | sed 's/^/> /'
+  echo
 
   if [ ${attempt} == ${max_attempts} ]; then
     echo Failed after ${attempt} attempts. >&2
