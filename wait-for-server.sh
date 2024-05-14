741a16217ea6b9beae59696b5f2696be67b3d5c2
Mike Pennisi <mike@mikepennisi.com>
2024-05-14 18:07:04 -0400

    Add more logging

--- wait-for-server.sh
+++ wait-for-server.sh
@@ -33,3 +33,23 @@ for attempt in $(seq ${max_attempts}); do
     exit 1
   fi
 done
+
+# Determine if the "ready" state is stable by repeatedly querying after
+# initial confirmation.
+for attempt in $(seq 5); do
+  echo "Testing for server availability (trial ${attempt} of ${max_attempts})."
+
+  ./bin/at-driver serve 2> ${temp_file} &
+  sleep 3
+  say This text came from the say command.
+  sleep 1
+  kill -9 %%
+
+  if grep --silent 'This text came from the say command.' ${temp_file}; then
+    echo The server is ready. Text observed:
+    cat ${temp_file} | sed 's/^/> /'
+  else
+    echo The server is not ready. Text observed:
+    cat ${temp_file} | sed 's/^/> /'
+  fi
+done
