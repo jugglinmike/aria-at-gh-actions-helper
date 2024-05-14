024bc566ac87daca3c7f016444631b8221adb84c
Mike Pennisi <mike@mikepennisi.com>
2024-05-14 18:33:47 -0400

    Remove references to SafariDriver

--- run-tester.sh
+++ run-tester.sh
@@ -2,16 +2,16 @@
 
 set -euo pipefail
 
-/usr/bin/safaridriver --port 4444 > webdriver.log 2>&1 &
-
-safaridriver_pid=$!
+# When run in macOS, the test harness does not use WebDriver because
+# SafariDriver's "glass pane" feature interferes with testing. Provide a valid
+# URL simply as a placeholder.
+url_placeholder=http://example.com
 
 aria-at-automation-driver/package/bin/at-driver serve --port 3031 > at-driver.log 2>&1 &
 
 atdriver_pid=$!
 
 function clean_up {
-  kill -9 ${safaridriver_pid} || true
   kill -9 ${atdriver_pid} || true
 }
 trap clean_up EXIT
@@ -19,7 +19,7 @@ trap clean_up EXIT
 node aria-at-automation-harness/bin/host.js run-plan \
   --plan-workingdir aria-at/build/${ARIA_AT_WORK_DIR} \
   --debug \
-  --agent-web-driver-url=http://127.0.0.1:4444 \
+  --agent-web-driver-url=${url_placeholder} \
   --agent-at-driver-url=ws://127.0.0.1:3031/session \
   --reference-hostname=127.0.0.1 \
   --agent-web-driver-browser=${BROWSER} \
